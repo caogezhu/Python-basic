634a1a6619e0275ab5456b510af8183e97150d07
caogezhu <caogezhu@qq.com>
2026-06-30 23:50:30 +0800

    新增流程控制语句-for循环-案例(range)

--- 08.for循环.py
+++ 08.for循环.py
@@ -1,8 +1,23 @@
 # for循环：遍历输入的字符串
 
-msg = input("请输入需要遍历的字符串：")
+# msg = input("请输入需要遍历的字符串：")
+#
+# for c in msg:
+#     print(f"元素：{c}")
+# else:
+#     print("循环结束！")
 
-for c in msg:
-    print(f"元素：{c}")
-else:
-    print("循环结束！")
+# 计算1-100奇数和
+
+total = 0
+for i in range(1,101,2):
+    total += i
+
+print(f"1-100奇数和：{i}")
+
+# 计算100-500奇数和
+total = 0
+for i in range(100,501,2):
+    total += i
+
+print(f"100-500奇数和：{i}")
