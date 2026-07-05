f5882fef167ac73a8e7a02789fca407dfb853f02
caogezhu <caogezhu@qq.com>
2026-07-05 23:29:53 +0800

    新增核心语法-数据存储与运算-输入与输出-更多案例

--- 03.输入与输出.py
+++ 03.输入与输出.py
@@ -4,15 +4,21 @@
 # else:
 #     print("不可以打游戏")
 
-# 案例：银行卡ATM取款
-total = 10000 #总金额
+# # 案例：银行卡ATM取款
+# total = 10000 #总金额
+#
+# # 1.输入密码
+# password = input("请输入您的银行卡密码：")
+# print(f"密码正确，{password}")
+#
+# # 2.输入取款金额
+# num = input("请输入您的取款金额：")
+#
+# # 3.计算余额并输出 --> num 转为 int类型 ---> int()
+# print(f"取款后银行卡余额为：{total - int(num)}")
 
-# 1.输入密码
-password = input("请输入您的银行卡密码：")
-print(f"密码正确，{password}")
+# 计算两数之和，输出到控制台
+num1 = float(input("请输入第一个数："))
+num2 = float(input("请输入第二个数："))
 
-# 2.输入取款金额
-num = input("请输入您的取款金额：")
-
-# 3.计算余额并输出 --> num 转为 int类型 ---> int()
-print(f"取款后银行卡余额为：{total - int(num)}")
+print(f"两数之和为：{num1 + num2}")
