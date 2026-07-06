bb3acc0243bf65a1b109084142daf7a624ae3e96
caogezhu <caogezhu@qq.com>
2026-07-06 21:00:49 +0800

    新增核心语法-流程控制语句-循环-综合案例(条件)(break与contiue)

--- 09.循环案例.py
+++ 09.循环案例.py
@@ -1,10 +1,15 @@
+i = 0
 while True:
     username = input("请输入正确的用户名：")
     password = input("请输入正确的密码:")
 
     if username == "" or password == "":
         print("用户名或密码为空，请重新输入！")
+        i+=1
         continue
+    if i == 4:
+        print("输入错误累计5次，禁止操作！")
+        break
     if username == "admin" and password == "123456":
         print("登录成功！进入B站首页！")
         break
@@ -16,3 +21,4 @@ while True:
         break
     else:
         print("用户名或密码错误！请重新输入！")
+        i+=1
