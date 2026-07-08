bc79ba36ed937d92bb66c4eadafdb42a3a8a8a60
caogezhu <caogezhu@qq.com>
2026-07-08 19:32:23 +0800

    Revert "修改旧文件9.list.py为新文件11.数据容器-list.py"
    This reverts commit 1bf4d586dd0c111f8249a8c4f6f818ee51532219.

---
+++ 9.list.py
@@ -0,0 +1,13 @@
+shopping_list = []
+shopping_list.append("零食")
+shopping_list.append("饮料")
+shopping_list.append("玩的")
+print(shopping_list)
+shopping_list.remove("玩的")
+print(shopping_list)
+print(shopping_list[0])
+
+price = [165,100,561]
+print(min(price))
+print(max(price))
+print(sorted(price))
