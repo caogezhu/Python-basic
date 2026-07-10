d58be299e195f8f08d10a6c0bdbdecb365083e24
caogezhu <caogezhu@qq.com>
2026-07-10 18:55:30 +0800

    新增核心语法-数据容器-列表list-习题2

--- 11.数据容器-list.py
+++ 11.数据容器-list.py
@@ -156,18 +156,22 @@ from hmac import new
 
 
 #合并列表后去重并排序
-list1 = ['M', 'A', 'C', 'E', 'F', 'G', 'H', 'L', 'N', 'I', 'J', 'K', 'O']
-list2 = ['X', 'Z', 'T', 'Y', 'D', 'E', 'F', 'G']
-list3 = ['W', 'A', 'S', 'D']
+# list1 = ['M', 'A', 'C', 'E', 'F', 'G', 'H', 'L', 'N', 'I', 'J', 'K', 'O']
+# list2 = ['X', 'Z', 'T', 'Y', 'D', 'E', 'F', 'G']
+# list3 = ['W', 'A', 'S', 'D']
+#
+# num_list = list1.copy()
+# for num in list2 + list3:
+#     if num not in num_list:
+#         num_list.append(num)
+# num_list.sort()
+# print(num_list)
 
-num_list = list1.copy()
-for num in list2 + list3:
-    if num not in num_list:
-        num_list.append(num)
-num_list.sort()
-print(num_list)
-
-
+# 将列表中能被3或5整除的数提出来，获取其平方，组成一个新列表
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+         17, 18,19, 20,21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+new_list = [i**2 for i in list1 if i%3==0 or i%5==0]
+print(new_list)
 
 
 
