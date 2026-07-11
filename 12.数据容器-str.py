da42d754b4ae9941133dc1a6ef298a839a2b8c19
caogezhu <caogezhu@qq.com>
2026-07-11 15:46:46 +0800

    新增核心语法-数据容器-字符串str-案例

--- 12.数据容器-str.py
+++ 12.数据容器-str.py
@@ -22,40 +22,80 @@
 # print(s[::-1])
 
 
-# ---------------------- 字符串常用方法 ----------------------
-s = "     Hello-Python-Hello-World     "
+# # ---------------------- 字符串常用方法 ----------------------
+# s = "     Hello-Python-Hello-World     "
+#
+# # find() 查找指定字符串第一次出现的索引位置
+# index = s.find("-")
+# print(index)
+#
+# # count() 统计子字符串在指定字符串中出现的次数
+# c = s.count("o")
+# print(c)
+#
+# # upper() 转为大写
+# su = s.upper()
+# print(su)
+#
+# # lower() 转为小写
+# sl = s.lower()
+# print(sl)
+#
+# # split() 将字符串按照指定字符串切割，返回列表
+# slist = s.split("-")
+# print(slist)
+#
+# # strip() 去除字符串两端的空格
+# ss = s.strip()
+# print(ss)
+#
+# # replace() 将字符串中的指定字串替换为新的内容
+# sr = s.replace("-", "_")
+# print(sr)
+#
+# # startswith()/endswith() 判断字符串是否是以指定的字符串开头 / 结尾，返回布尔值
+# print(s.startswith("Hello"))
+# print(s.endswith("Python"))
+#
+# print("--------------------------")
+# print(s)
 
-# find() 查找指定字符串第一次出现的索引位置
-index = s.find("-")
-print(index)
 
-# count() 统计子字符串在指定字符串中出现的次数
-c = s.count("o")
-print(c)
+# -----------------------字符串案例----------------------
+# 输入邮箱，判断邮箱格式是否合法(只有一个@，至少一个.)
 
-# upper() 转为大写
-su = s.upper()
-print(su)
+# 方法一:
+# 1.接受用户的编码
+# mail = input("请输入邮箱：")
+#
+# if mail.count("@")==1 and mail.count(".")>0:
+#     print("邮箱合法！")
+# else:
+#     print("邮箱非法！")
 
-# lower() 转为小写
-sl = s.lower()
-print(sl)
+# 方法二: in 运算符 --> 判断字串是否在字符串中，存在，返回True，否则false
+# 1.接受用户的编码
+mail = input("请输入邮箱：")
 
-# split() 将字符串按照指定字符串切割，返回列表
-slist = s.split("-")
-print(slist)
+if mail.count("@")==1 and "." in mail:
+    print("邮箱合法！")
+else:
+    print("邮箱非法！")
 
-# strip() 去除字符串两端的空格
-ss = s.strip()
-print(ss)
 
-# replace() 将字符串中的指定字串替换为新的内容
-sr = s.replace("-", "_")
-print(sr)
 
-# startswith()/endswith() 判断字符串是否是以指定的字符串开头 / 结尾，返回布尔值
-print(s.startswith("Hello"))
-print(s.endswith("Python"))
 
-print("--------------------------")
-print(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
