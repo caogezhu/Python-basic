8eaaa68ad395c9fbbbd959f03d1af545cb953519
caogezhu <caogezhu@qq.com>
2026-09-08 22:40:39 +0800

    新增核心语法-数据容器-字典dict案例2

--- 15.数据容器-dict-介绍.py
+++ 15.数据容器-dict-介绍.py
@@ -72,70 +72,186 @@ from itertools import count
 
 结构： shopping_cart = {"Meta80": {"price": 6999, "num": 2}, "鼠标": {...}}
 """
-shopping_cart = {}
-menu = """
-############## 购物车管理系统 ###############
-#               1.添加购物车                #
-#               2.修改购物车                #
-#               3.删除购物车                #
-#               4.查询购物车                #
-#               5.退出购物车                #
-###########################################
+# shopping_cart = {}
+# menu = """
+# ############## 购物车管理系统 ###############
+# #               1.添加购物车                #
+# #               2.修改购物车                #
+# #               3.删除购物车                #
+# #               4.查询购物车                #
+# #               5.退出购物车                #
+# ###########################################
+# """
+#
+# print("欢迎使用购物车管理系统~")
+#
+# while True:
+#     # 1.制作菜单
+#     print(menu)
+#
+#     # 2.执行的具体操作
+#     choice = input("请选择要执行的操作(1-5):")
+#
+#     match choice:
+#         case "1":  # 1.添加购物车
+#             goods_name = input("请输入商品名称：")
+#             goods_price = float(input("请输入商品价格："))
+#             goods_num = int(input("请输入商品数量："))
+#
+#             # 如果商品存在，则不执行添加，提示信息
+#             if goods_name in shopping_cart:
+#                 print("该商品已存在，请重新选择！")
+#             else:
+#                 shopping_cart[goods_name] = {"price": goods_price, "num": goods_num}
+#                 print("商品添加完毕~")
+#         case "2":  # 2.修改购物车
+#             goods_name = input("请输入要修改的商品名称：")
+#             # 如果商品不存在，则提示错误信息，重新选择
+#             if goods_name not in shopping_cart:
+#                 print("该商品不存在，请重新选择！")
+#                 continue
+#
+#             goods_price = float(input("请输入商品最新的价格："))
+#             goods_num = int(input("请输入商品最新的数量："))
+#
+#             shopping_cart[goods_name] = {"price": goods_price, "num": goods_num}
+#             print("商品修改完毕~")
+#         case "3":  # 3.删除购物车
+#             goods_name = input("请输入要删除的商品名称：")
+#
+#             # 如果商品不存在，则提示错误信息，重新选择
+#             if goods_name not in shopping_cart:
+#                 print("该商品不存在，请重新选择！")
+#             else:
+#                 del shopping_cart[goods_name]
+#                 print("商品删除完毕~")
+#         case "4":  # 4.查询购物车
+#             for goods_name in shopping_cart.keys():
+#                 goods_info = shopping_cart[goods_name]
+#                 print(f"商品名称：{goods_name},商品价格：{goods_info['price']},商品数量：{goods_info['num']}")
+#         case "5":  # 5.退出购物车
+#             print("Bye ~")
+#             break;
+#         case _:  # 匹配其它操作
+#             print("非法操作，不支持！")
+
+
+"""
+基于现有知识开发一个教务管理系统
+ 
+开发一个教务管理系统，在该系统中可以维护和管理学员的成绩信息，具体需求如下：
+1．添加学生信息：根据提示录入学生姓名、语文、数学、英语成绩，录入完成保存到系统中。
+ 
+2．修改学生信息：要求输入要修改的学生姓名，然后再提示输入语文、数学、英语成绩，输入完成后修改学员信息。
+ 
+3．删除学生信息：要求输入要删除的学生姓名，根据姓名删除学生信息。
+ 
+4．查询学生信息：要求输入要查询的学生姓名，根据姓名查询学生信息并输出。
+ 
+5．列出所有学生：遍历所有学生信息并输出。
+ 
+6．统计班级成绩：统计班级语文、数学、英语成绩的最高分、最低分、平均分，以及语文、数学、英语最高分和最低分的学员姓名。
+ 
+7．退出系统。
 """
 
-print("欢迎使用购物车管理系统~")
-
+menu = """
+############## 购物车管理系统 ###############
+#               1.添加学生信息                #
+#               2.修改学生信息                #
+#               3.删除学生信息                #
+#               4.查询学生信息                #
+#               5.列出所有学生                #
+#               6.统计班级成绩                #
+#               7.退出系统                   #
+###########################################
+"""
+student_news = {"cd":{"chinese":15,"math":16,"english":76},"ln":{"chinese":42,"math":76,"english":68}}
 while True:
     # 1.制作菜单
     print(menu)
 
     # 2.执行的具体操作
-    choice = input("请选择要执行的操作(1-5):")
+    choice = input("请选择要执行的操作(1-7):")
 
     match choice:
-        case "1":  # 1.添加购物车
-            goods_name = input("请输入商品名称：")
-            goods_price = float(input("请输入商品价格："))
-            goods_num = int(input("请输入商品数量："))
-
-            # 如果商品存在，则不执行添加，提示信息
-            if goods_name in shopping_cart:
-                print("该商品已存在，请重新选择！")
+        case "1":
+            s_name = input("请输入学生姓名：")
+            s_chinese = float(input("请输入语文成绩："))
+            s_math = float(input("请输入数学成绩："))
+            s_english = float(input("请输入英语成绩："))
+            # 如果学生存在，则不执行添加，提示信息
+            if s_name in student_news:
+                print("该学生已存在，请重新选择！")
             else:
-                shopping_cart[goods_name] = {"price": goods_price, "num": goods_num}
-                print("商品添加完毕~")
-        case "2":  # 2.修改购物车
-            goods_name = input("请输入要修改的商品名称：")
-            # 如果商品不存在，则提示错误信息，重新选择
-            if goods_name not in shopping_cart:
-                print("该商品不存在，请重新选择！")
+                student_news[s_name] = {"chinese": s_chinese, "math": s_math, "english": s_english}
+                print(f"添加【{s_name}】添加完毕~")
+        case "2":
+            s_name = input("请输入需更改的学生姓名：")
+            if s_name not in student_news:
+                print("该学生不存在，请重新输入！")
                 continue
+            s_chinese = float(input("请输入正确的语文成绩："))
+            s_math = float(input("请输入正确的数学成绩："))
+            s_english = float(input("请输入正确的英语成绩："))
+            student_news[s_name] = {"chinese":s_chinese, "math":s_math, "english":s_english}
+            print("学生信息修改完毕!")
+        case "3":
+            s_name = input("请输入需删除的学生姓名：")
+            if s_name not in student_news:
+                print("该学生不存在，请重新输入！")
+                continue
+            del student_news[s_name]
+            print("学生删除完毕！")
+        case "4":
+            s_name = input("请输入需查询的学生姓名：")
+            if s_name not in student_news:
+                print("该学生不存在，请重新输入！")
+                continue
+            s_cj = student_news[s_name]
+            print(f"学生名称：{s_name},语文成绩：{s_cj["chinese"]},数学成绩：{s_cj["math"]},英语成绩：{s_cj["english"]}")
+        case "5":
+            for s_name,s_cj in student_news.items():
+                print(f"学生名称：{s_name},语文成绩：{s_cj["chinese"]},数学成绩：{s_cj["math"]},英语成绩：{s_cj["english"]}")
+        case "6":
+            if not student_news:
+                print("暂无学生数据！")
+                break
 
-            goods_price = float(input("请输入商品最新的价格："))
-            goods_num = int(input("请输入商品最新的数量："))
+            chi_list = []
+            mat_list = []
+            eng_list = []
 
-            shopping_cart[goods_name] = {"price": goods_price, "num": goods_num}
-            print("商品修改完毕~")
-        case "3":  # 3.删除购物车
-            goods_name = input("请输入要删除的商品名称：")
+            for name, s in student_news.items():
+                chi_list.append([s["chinese"], name])
+                mat_list.append([s["math"], name])
+                eng_list.append([s["english"], name])
 
-            # 如果商品不存在，则提示错误信息，重新选择
-            if goods_name not in shopping_cart:
-                print("该商品不存在，请重新选择！")
-            else:
-                del shopping_cart[goods_name]
-                print("商品删除完毕~")
-        case "4":  # 4.查询购物车
-            for goods_name in shopping_cart.keys():
-                goods_info = shopping_cart[goods_name]
-                print(f"商品名称：{goods_name},商品价格：{goods_info['price']},商品数量：{goods_info['num']}")
-        case "5":  # 5.退出购物车
-            print("Bye ~")
-            break;
-        case _:  # 匹配其它操作
-            print("非法操作，不支持！")
+            # 语文
+            chi_list.sort()
+            max_chi, max_chi_name = chi_list[-1]
+            min_chi, min_chi_name = chi_list[0]
+            avg_chi = sum(i[0] for i in chi_list) / len(chi_list)
 
+            # 数学
+            mat_list.sort()
+            max_mat, max_mat_name = mat_list[-1]
+            min_mat, min_mat_name = mat_list[0]
+            avg_mat = sum(i[0] for i in mat_list) / len(mat_list)
 
+            # 英语
+            eng_list.sort()
+            max_eng, max_eng_name = eng_list[-1]
+            min_eng, min_eng_name = eng_list[0]
+            avg_eng = sum(i[0] for i in eng_list) / len(eng_list)
+
+            print("====班级统计====")
+            print(f"语文 最高{max_chi}({max_chi_name}) 最低{min_chi}({min_chi_name}) 平均{avg_chi:.2f}")
+            print(f"数学 最高{max_mat}({max_mat_name}) 最低{min_mat}({min_mat_name}) 平均{avg_mat:.2f}")
+            print(f"英语 最高{max_eng}({max_eng_name}) 最低{min_eng}({min_eng_name}) 平均{avg_eng:.2f}")
+        case "7":
+            print("感谢使用！")
+            break
 
 
 
