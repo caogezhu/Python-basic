8a0f5c2b16f0e4c70ecf406665a0000ccc52f80d
caogezhu <caogezhu@qq.com>
2026-09-10 22:29:58 +0800

    新增核心语法-函数基础-函数说明文档

--- 16.函数基础.py
+++ 16.函数基础.py
@@ -17,13 +17,24 @@ out_line()
 
 # 函数2：计算长方形的面积 -- 长，宽
 def rectangle_area(l,w):
+    """
+    根据长、宽计算长方形的面积
+    :param l: 长度
+    :param w: 宽度
+    :return: 长方形的面积
+    """
     area = l * w
     return area
-
+help(rectangle_area)
 print(rectangle_area(20, 10))
 
 # 函数3：计算圆的面积，周长 -- 半径 ----> 如果返回值有多个，多个返回值之间逗号分隔 ----> 多个返回值会封装到元组之中
 def circle_area_len(r):
+    """
+    根据半径计算圆的面积，周长
+    :param r: 半径
+    :return: 圆的面积，周长
+    """
     return round(3.14**r,1),round(2*3.14**r,1)
 al = circle_area_len(10)
 print(al)
