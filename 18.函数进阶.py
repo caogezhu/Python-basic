f6be18ba563d3142c2292e1e5b3c9a0262f2704e
caogezhu <caogezhu@qq.com>
2026-09-12 16:15:07 +0800

    新增核心语法-函数进阶-不定长参数

--- 18.函数进阶.py
+++ 18.函数进阶.py
@@ -42,20 +42,60 @@
 # stu = reg_stu("李慕婉", 20, gender="女", city="北京")
 # print(stu)
 
-# ------------------------ 函数 - 默认参数 ------------------------
-# 定义函数
-def reg_stu(name, age, gender="男", city="北京"):
-    print(f"注册成功，姓名: {name}, 年龄: {age}, 性别: {gender}, 城市: {city}")
-    return {"name": name, "age": age, "gender": gender, "city": city}
+# # ------------------------ 函数 - 默认参数 ------------------------
+# # 定义函数
+# def reg_stu(name, age, gender="男", city="北京"):
+#     print(f"注册成功，姓名: {name}, 年龄: {age}, 性别: {gender}, 城市: {city}")
+#     return {"name": name, "age": age, "gender": gender, "city": city}
+#
+# # 调用函数
+# stu = reg_stu("王林", 20)
+# print(stu)
+#
+# stu = reg_stu("李慕婉", 18, gender="女")
+# print(stu)
+#
+# stu = reg_stu("韩立", 22, city="上海")
+# print(stu)
+
+# ------------------------ 函数 - 不定长参数(*args --> 元组) ------------------------
+# *args:不定长位置参数，函数调用时，通过位置参数传递多个参数封装到一个元组(tuple)
+# 需求：根据传入的这批数据，计算这批数据的最小值，最大值，平均值
+def calc_data(*args):
+    min_data = min(args)
+    max_data = max(args)
+    avg_data = sum(args) / len(args)
+    return min_data, max_data, round(avg_data, 1)
 
 # 调用函数
-stu = reg_stu("王林", 20)
-print(stu)
+print(calc_data(2, 7, 9, 10, 45))
+print(calc_data(2, 7, 9, 10, 45, 73, 37, 93, 92, 111, 222))
 
-stu = reg_stu("李慕婉", 18, gender="女")
-print(stu)
+# ------------------------ 函数 - 不定长参数(*args,**kwargs) ------------------------
+# **kwargs:不定长关键字参数，函数调用时，通过关键字传递多个参数封装到一个字典(dict)
+# 需求：根据传入的这批数据，计算这批数据的最小值，最大值，平均值
+def calc_data(*args, **kwargs):
+    """
+    根据传入的这批数据，计算这批数据的最小值，最大值，平均值
+    :param args: 不定长位置参数，需要计算的这批数据
+    :param kwargs: 不定长关键字参数
+        round: 保留的小数位个数
+        print: 是否打印输出
+    :return: 最小值，最大值，平均值
+    """
+    min_data = min(args)
+    max_data = max(args)
+    avg_data = sum(args) / len(args)
 
-stu = reg_stu("韩立", 22, city="上海")
-print(stu)
+    if kwargs.get("round") is not None:
+        avg_data = round(avg_data, kwargs.get("round"))
+
+    if kwargs.get("print"):
+        print(f"计算出来的最小值：{min_data}, 最大值: {max_data}, 平均值:{avg_data}")
+
+    return min_data, max_data, avg_data
 
 
+# 调用函数
+# print(calc_data(2, 7, 9, 10, 45, round=3, print=True))
+print(calc_data(2, 7, 9, 10, 45, 73, 37, 93, 92, 111, 222, round=4, print=False))
