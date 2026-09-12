bda4c4231e778989feb35aceeea09bf35c8c134b
caogezhu <caogezhu@qq.com>
2026-09-12 16:01:01 +0800

    新增核心语法-函数进阶-默认参数

--- 18.函数进阶.py
+++ 18.函数进阶.py
@@ -21,25 +21,41 @@
 #
 # print("num = ", num)  # 10000
 
-# ------------------------ 函数 - 传参方式 ------------------------
+# # ------------------------ 函数 - 传参方式 ------------------------
+# # 定义函数
+# def reg_stu(name, age, gender, city):
+#     print(f"注册成功，姓名: {name}, 年龄: {age}, 性别: {gender}, 城市: {city}")
+#     return {"name": name, "age": age, "gender": gender, "city": city}
+#
+# # 传参方式一：位置参数
+# stu = reg_stu("张三", 18, "男", "北京")
+# print(stu)
+#
+# # 传参方式二：关键字参数
+# stu = reg_stu(name="王林", age=28, gender="男", city="北京")
+# print(stu)
+#
+# stu = reg_stu(age=20, gender="女", city="北京", name="李慕婉")
+# print(stu)
+#
+# # 传参方式三：位置参数 + 关键字参数 ---> 位置参数在前，关键字参数在后
+# stu = reg_stu("李慕婉", 20, gender="女", city="北京")
+# print(stu)
+
+# ------------------------ 函数 - 默认参数 ------------------------
 # 定义函数
-def reg_stu(name, age, gender, city):
+def reg_stu(name, age, gender="男", city="北京"):
     print(f"注册成功，姓名: {name}, 年龄: {age}, 性别: {gender}, 城市: {city}")
     return {"name": name, "age": age, "gender": gender, "city": city}
 
-# 传参方式一：位置参数
-stu = reg_stu("张三", 18, "男", "北京")
+# 调用函数
+stu = reg_stu("王林", 20)
 print(stu)
 
-# 传参方式二：关键字参数
-stu = reg_stu(name="王林", age=28, gender="男", city="北京")
+stu = reg_stu("李慕婉", 18, gender="女")
 print(stu)
 
-stu = reg_stu(age=20, gender="女", city="北京", name="李慕婉")
-print(stu)
-
-# 传参方式三：位置参数 + 关键字参数 ---> 位置参数在前，关键字参数在后
-stu = reg_stu("李慕婉", 20, gender="女", city="北京")
+stu = reg_stu("韩立", 22, city="上海")
 print(stu)
 
 
