181bfd5ee8ed9176cf797882f14d2c3cd52b1ee6
caogezhu <caogezhu@qq.com>
2026-09-13 16:18:46 +0800

    新增核心语法-函数进阶-函数类型(函数作为参数)

---
+++ 19.函数进阶2.py
@@ -0,0 +1,51 @@
+# 函数的参数类型
+# 加
+def add(x, y):
+    return x + y
+
+# 减
+def subtract(x, y):
+    return x - y
+
+# 乘
+def multiply(x, y):
+    return x * y
+
+# 除
+def divide(x, y):
+    return x / y
+
+# 计算
+def calc(x, y, oper):
+    return oper(x, y)
+
+print(calc(10, 2, multiply ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
