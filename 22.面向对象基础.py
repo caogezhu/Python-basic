da79d7737c0e06bc7b14b4b29f69ccde7a71cdfa
caogezhu <caogezhu@qq.com>
2026-09-21 20:06:12 +0800

    新增核心语法-面向对象基础-实例方法

--- 22.面向对象基础.py
+++ 22.面向对象基础.py
@@ -17,9 +17,27 @@
 
 
 # 定义类
-class Car:  # 2 用法
-    # __init__ 方法是初始化的方法，会在对象创建时自动调用，可以在该方法中为对象设置对应的属性；
-    # self: 是第一个参数，表示当前所创建出来的实例对象
+# class Car:  # 2 用法
+#     # __init__ 方法是初始化的方法，会在对象创建时自动调用，可以在该方法中为对象设置对应的属性；
+#     # self: 是第一个参数，表示当前所创建出来的实例对象
+#     def __init__(self, c_color, c_brand, c_name, c_price):
+#         self.color = c_color
+#         self.brand = c_brand
+#         self.name = c_name
+#         self.price = c_price
+#         print("Car 类型的对象初始化完毕，对象属性已经添加完毕 .")
+#
+#
+# # 创建对象
+# c1 = Car(c_color="红色", c_brand="BMW", c_name="X7", c_price=800000)
+# print(c1.__dict__)
+#
+# c2 = Car(c_color="白色", c_brand="奔驰", c_name="E300", c_price=450000)
+# print(c2.__dict__)
+
+
+# --------------------------- 定义类 实例方法 ---------------------------
+class Car:
     def __init__(self, c_color, c_brand, c_name, c_price):
         self.color = c_color
         self.brand = c_brand
@@ -27,10 +45,42 @@ class Car:  # 2 用法
         self.price = c_price
         print("Car 类型的对象初始化完毕，对象属性已经添加完毕 .")
 
+    # 定义实例方法
+    def running(self):
+        print(f"{self.brand} {self.name} 正在高速行驶中....")
 
-# 创建对象
-c1 = Car(c_color="红色", c_brand="BMW", c_name="X7", c_price=800000)
-print(c1.__dict__)
+    def total_price(self,discount,rate):
+        """
+        计算提车的总费用，包含两个部分：车的价格，税费
+        :param discount: 折扣
+        :param rate: 税率
+        :return: 提车的总费用
+        """
+        total_cost = self.price * rate + discount * self.price
+        return total_cost
 
-c2 = Car(c_color="白色", c_brand="奔驰", c_name="E300", c_price=450000)
-print(c2.__dict__)
+# 测试
+c1 = Car("红色", "BMW", "X7", 800000)
+
+# 调用对象中的方法
+c1.running()
+
+total = c1.total_price(0.9, 0.1)
+print("提车的总费用为：", total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
